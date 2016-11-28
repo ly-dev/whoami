2e896a86ec3be2c54768c2bef3c6dd6fe6e441b5
Yong Li <dev@liyong.cn>
2016-11-28 16:08:42 +0000

    updated

--- www/js/root/root.js
+++ www/js/root/root.js
@@ -16,3 +16,38 @@ angular.module('whoamiApp')
                 controller: 'RootCtrl'
             });
     });
+
+            // start
+        AppSignUp.prepareAppContext().then(function (context) {
+            AppLog.debug('Device is activated');
+            AppHelper.showLoading();
+            AppSync.syncAll().then(function (result) {
+                AppHelper.hideLoading();
+            });
+        }, function (data) {
+            AppLog.debug('Device is not activated');
+            AppSignUp.showLogin({
+                scope: $scope,
+                startModal: 'signUp'
+            });
+        });
+        
+        
+                service.getDeviceInfo = function () {
+            var info = (APP_CONTEXT.env == 'web' ? {
+                'cordova': 'n/a',
+                'model': 'web',
+                'platform': 'browser',
+                'version': (navigator ? navigator.userAgent :'n/a'),
+                'uuid': (APP_CONTEXT.activeUser ? APP_CONTEXT.activeUser.name : 'unknown'),
+            } : $cordovaDevice.getDevice());
+
+            info.app_version = APP_CONFIG.APP_VERSION;
+            info.window = {
+                innerWidth: window.innerWidth,
+                innerHeight: window.innerHeight
+            };
+
+            return info;
+        };
+
